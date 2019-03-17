ad345925aa1223605c17531c801a61888f21f129
moridi <m.moridi.2009@gmail.com>
2019-03-17 15:21:57 +0330

    Tokenize HTTP request by new lines

--- ProxyServer.py
+++ ProxyServer.py
@@ -1,12 +1,30 @@
 import json
 import socket
 
-class ProxyServer():
-    def parseJsonFile(self, fileName):
+class Parser(object):
+    @staticmethod
+    def parseJsonFile(fileName):
         jsonFile = open(fileName, "r")
         contents = jsonFile.read()
-        return json.loads(contents)
+        return json.loads(contents)   
 
+    @staticmethod
+    def parseHttpMessage(message):
+        parsedData = []
+        line = ""
+
+        for character in message:
+            if (chr(character) == '\r'):
+                continue
+            if (chr(character) == '\n'):
+                parsedData.append(line)
+                line = ""
+                continue
+            line += chr(character)
+
+        return parsedData
+
+class ProxyServer(object):
     def setupTcpConnection(self):
         MAX_CLIENT_NUMBER = 5
 
@@ -16,23 +34,22 @@ class ProxyServer():
         return proxySocket
 
     def __init__(self, fileName):
-        
-        self.config = self.parseJsonFile(fileName)
+        self.config = Parser.parseJsonFile(fileName)
         self.proxySocket = self.setupTcpConnection()
+        self.HttpMessage = None
 
     def run(self):
-        # while 1:
-        # self.proxySocket.close()
+        MAX_BUFFER_SIZE = 1024
 
         (clientsocket, address) = self.proxySocket.accept()
-
-        # while True:
-        data = clientsocket.recv(1024)
-        # clientsocket.re
-        print(data)
+        data = clientsocket.recv(MAX_BUFFER_SIZE)
         if not data:
             print >>sys.stderr, 'no more data from', client_address
-            # break
+
+        self.httpMessage = Parser.parseHttpMessage(data)
+
+        for x in self.httpMessage:
+            print(x)
 
         self.proxySocket.close()
 
