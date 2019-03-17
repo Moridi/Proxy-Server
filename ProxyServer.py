1533a99f630248e2dc53d63a7ace95645c7262ee
moridi <m.moridi.2009@gmail.com>
2019-03-17 17:36:35 +0330

    Remove Proxy-Connection header from HTTP request

--- ProxyServer.py
+++ ProxyServer.py
@@ -59,25 +59,56 @@ class ProxyServer(object):
         self.proxySocket = self.setupTcpConnection()
 
     def changeHttpVersion(self):
-        httpVersionIndex = self.httpMessage[0][1].find("HTTP/1.")
-        self.httpMessage[0] = (self.httpMessage[0][0],\
-                self.httpMessage[0][1][ : httpVersionIndex] +\
-                "HTTP/1.0" + self.httpMessage[0][1][httpVersionIndex + 8 : ])
+        REQUEST_LINE = 0
+        FIRST_PART = 0
+        HTTP_VERSION_PART = 1
+        HTTP_VERSION = "HTTP/1.0"
+
+        httpVersionIndex = self.httpMessage[REQUEST_LINE][HTTP_VERSION_PART].find(" ") + 1
+
+        self.httpMessage[REQUEST_LINE] = (\
+                self.httpMessage[REQUEST_LINE][FIRST_PART],\
+                self.httpMessage[REQUEST_LINE][HTTP_VERSION_PART][ : httpVersionIndex] +\
+                HTTP_VERSION + self.httpMessage[REQUEST_LINE][HTTP_VERSION_PART][\
+                httpVersionIndex + len(HTTP_VERSION) : ])
 
     def changeUrl(self):
-        spaceIndex = self.httpMessage[0][1].find(" ")
+        REQUEST_LINE = 0
+        HOST_NAME_LINE = 1
+        FIRST_PART = 0
+        URL_PART = 1
+        HTTP_PART = "http:/"
 
-        self.httpMessage[0] = (self.httpMessage[0][0], self.httpMessage[0][1][6 + len(self.httpMessage[1][1]) : ])
+        spaceIndex = self.httpMessage[REQUEST_LINE][URL_PART].find(" ")
+
+        self.httpMessage[REQUEST_LINE] = (self.httpMessage[REQUEST_LINE][FIRST_PART],\
+                self.httpMessage[REQUEST_LINE][URL_PART][len(HTTP_PART) +\
+                len(self.httpMessage[HOST_NAME_LINE][URL_PART]) : ])
+
+    def getProxyConnectionIndex(self):
+        PROXY_CONNECTION = "Proxy-Connection"
+        FIELD_PART = 0
+
+        index = [i for i, x in enumerate(self.httpMessage) if(x[FIELD_PART] == PROXY_CONNECTION)]
+        if (len(index) == 0):
+            return -1
+        return index[0]
+
+    def removeProxyHeader(self):
+        index = self.getProxyConnectionIndex()
+        self.httpMessage.pop(index)
 
     def prepareRequest(self):
         self.changeHttpVersion()
         self.changeUrl()
+        self.removeProxyHeader()
 
     def run(self):
         MAX_BUFFER_SIZE = 1024
 
         (clientsocket, address) = self.proxySocket.accept()
         data = clientsocket.recv(MAX_BUFFER_SIZE)
+        
         self.httpMessage = Parser.parseHttpMessage(data)
         self.prepareRequest()
 
