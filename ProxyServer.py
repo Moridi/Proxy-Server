80e7722c6fb6b5997f1557be92eac846675e16c2
moridi <m.moridi.2009@gmail.com>
2019-03-20 12:20:53 +0330

    Increase number of clients on a single port

--- ProxyServer.py
+++ ProxyServer.py
@@ -1,10 +1,13 @@
 import socket
+import threading
 
 from Parser import Parser
 
+MAX_BUFFER_SIZE = 1024
+
 class ProxyServer(object):
     def setupTcpConnection(self):
-        MAX_CLIENT_NUMBER = 5
+        MAX_CLIENT_NUMBER = 30
 
         proxySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         proxySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -16,65 +19,65 @@ class ProxyServer(object):
         self.config = Parser.parseJsonFile(fileName)
         self.proxySocket = self.setupTcpConnection()
 
-    def changeHttpVersion(self):
+    def changeHttpVersion(self, httpMessage):
         REQUEST_LINE = 0
         FIRST_PART = 0
         HTTP_VERSION_PART = 1
         HTTP_VERSION = "HTTP/1.0"
 
-        httpVersionIndex = self.httpMessage[REQUEST_LINE][HTTP_VERSION_PART].find(" ") + 1
+        httpVersionIndex = httpMessage[REQUEST_LINE][HTTP_VERSION_PART].find(" ") + 1
 
-        self.httpMessage[REQUEST_LINE] = (\
-                self.httpMessage[REQUEST_LINE][FIRST_PART],\
-                self.httpMessage[REQUEST_LINE][HTTP_VERSION_PART][ : httpVersionIndex] +\
-                HTTP_VERSION + self.httpMessage[REQUEST_LINE][HTTP_VERSION_PART][\
+        httpMessage[REQUEST_LINE] = (\
+                httpMessage[REQUEST_LINE][FIRST_PART],\
+                httpMessage[REQUEST_LINE][HTTP_VERSION_PART][ : httpVersionIndex] +\
+                HTTP_VERSION + httpMessage[REQUEST_LINE][HTTP_VERSION_PART][\
                 httpVersionIndex + len(HTTP_VERSION) : ])
 
-    def changeUrl(self):
+    def changeUrl(self, httpMessage):
         REQUEST_LINE = 0
         HOST_NAME_LINE = 1
         FIRST_PART = 0
         URL_PART = 1
         HTTP_PART = "http:/"
 
-        spaceIndex = self.httpMessage[REQUEST_LINE][URL_PART].find(" ")
+        spaceIndex = httpMessage[REQUEST_LINE][URL_PART].find(" ")
 
-        self.httpMessage[REQUEST_LINE] = (self.httpMessage[REQUEST_LINE][FIRST_PART],\
-                self.httpMessage[REQUEST_LINE][URL_PART][len(HTTP_PART) +\
-                len(self.httpMessage[HOST_NAME_LINE][URL_PART]) : ])
+        httpMessage[REQUEST_LINE] = (httpMessage[REQUEST_LINE][FIRST_PART],\
+                httpMessage[REQUEST_LINE][URL_PART][len(HTTP_PART) +\
+                len(httpMessage[HOST_NAME_LINE][URL_PART]) : ])
 
-    def getProxyConnectionIndex(self):
+    def getProxyConnectionIndex(self, httpMessage):
         PROXY_CONNECTION = "Proxy-Connection"
         FIELD_PART = 0
 
-        index = [i for i, x in enumerate(self.httpMessage) if(x[FIELD_PART] == PROXY_CONNECTION)]
+        index = [i for i, x in enumerate(httpMessage) if(x[FIELD_PART] == PROXY_CONNECTION)]
         if (len(index) == 0):
             return -1
         return index[0]
 
-    def removeProxyHeader(self):
-        index = self.getProxyConnectionIndex()
-        self.httpMessage.pop(index)
+    def removeProxyHeader(self, httpMessage):
+        index = self.getProxyConnectionIndex(httpMessage)
+        httpMessage.pop(index)
 
-    def prepareRequest(self):
-        self.changeHttpVersion()
-        self.changeUrl()
-        self.removeProxyHeader()
+    def prepareRequest(self, httpMessage):
+        self.changeHttpVersion(httpMessage)
+        self.changeUrl(httpMessage)
+        self.removeProxyHeader(httpMessage)
 
-    def setupHttpConnection(self):
+    def setupHttpConnection(self, httpMessage):
         URL_PART = HOST_NAME_LINE = FIRST_CHAR = 1
         HTTP_PORT = 80
 
-        ipAddress = socket.gethostbyname(self.httpMessage[HOST_NAME_LINE][URL_PART][FIRST_CHAR : ])
+        ipAddress = socket.gethostbyname(httpMessage[HOST_NAME_LINE][URL_PART][FIRST_CHAR : ])
 
         httpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         httpSocket.connect((ipAddress, HTTP_PORT))
         
         return httpSocket
 
-    def getRequestMessage(self):
+    def getRequestMessage(self, httpMessage):
         message = ""
-        for i, x in enumerate(self.httpMessage):
+        for i, x in enumerate(httpMessage):
             if (i == 0):
                 message += x[0] + " " + x[1] + "\r\n"                
             else:
@@ -84,28 +87,45 @@ class ProxyServer(object):
 
         return message
 
-    def sendHttpRequest(self):
-        message = self.getRequestMessage()        
+    def sendHttpRequest(self, clientSocket, httpSocket, httpMessage):
+        message = self.getRequestMessage(httpMessage)     
 
-        self.httpSocket.sendall(bytes(message, 'utf-8'))
+        httpSocket.sendall(bytes(message, 'utf-8'))
 
         while True:
-            data = self.httpSocket.recv(1024)
+            data = httpSocket.recv(1024)
             if not data:
                 break
             print(str(data))
-            self.clientsocket.sendall(data)
+            clientSocket.sendall(data)
+
+    def proxyThread(self, clientSocket, clientAddress):
+            data = clientSocket.recv(MAX_BUFFER_SIZE)
+            
+            httpMessage = Parser.parseHttpMessage(data)
+            self.prepareRequest(httpMessage)
+
+            httpSocket = self.setupHttpConnection(httpMessage)
+            self.sendHttpRequest(clientSocket, httpSocket, httpMessage)
 
     def run(self):
-        MAX_BUFFER_SIZE = 1024
+        # while True:
 
-        (self.clientsocket, address) = self.proxySocket.accept()
+            # Establish the connection
+            (clientSocket, clientAddress) = self.proxySocket.accept() 
+            self.proxyThread(clientSocket, clientAddress)
+            # d = threading.Thread(target = self.proxyThread, args=(clientSocket, clientAddress))
+            # d.setDaemon(True)
+            # d.start()
 
-        data = self.clientsocket.recv(MAX_BUFFER_SIZE)
-        
-        self.httpMessage = Parser.parseHttpMessage(data)
-        self.prepareRequest()
+        # while True:
+            # (self.clientSocket, address) = self.proxySocket.accept()
 
-        self.httpSocket = self.setupHttpConnection()
-        self.sendHttpRequest()
-        self.proxySocket.close()
+            # data = self.clientSocket.recv(MAX_BUFFER_SIZE)
+            
+            # self.httpMessage = Parser.parseHttpMessage(data)
+            # self.prepareRequest()
+
+            # self.httpSocket = self.setupHttpConnection()
+            # self.sendHttpRequest()
+            # self.proxySocket.close()
